5cabe88983baede5450144953f368d7a7bd0c916
xanoosh <xanoosh@gmail.com>
2025-11-27 08:11:43 +0100

    Formelement field moved inside controller

--- src/components/Form/FormElement/FormElement.tsx
+++ src/components/Form/FormElement/FormElement.tsx
@@ -19,59 +19,57 @@ export default function FormElement({
   options,
 }: FormElementInterface) {
   return (
-    <Field>
-      <FieldLabel htmlFor={name}>{label}</FieldLabel>
-      <Controller
-        name={name}
-        control={control}
-        render={({ field, fieldState }) => (
-          <>
-            {variant === 'text' ? (
-              <Textarea
-                rows={4}
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState }) => (
+        <Field data-invalid={fieldState.invalid}>
+          <FieldLabel htmlFor={name}>{label}</FieldLabel>
+          {variant === 'text' ? (
+            <Textarea
+              rows={4}
+              id={name}
+              aria-invalid={fieldState.invalid}
+              {...field}
+            />
+          ) : null}
+          {variant === 'number' ? (
+            <Input
+              type="number"
+              id={name}
+              aria-invalid={fieldState.invalid}
+              {...field}
+              onChange={(e) => field.onChange(+e.target.value)}
+            />
+          ) : null}
+          {variant === 'select' && options ? (
+            <Select
+              aria-invalid={fieldState.invalid}
+              onValueChange={(value) => field.onChange(value)}
+              value={String(field.value)}
+            >
+              <SelectTrigger
                 id={name}
-                aria-invalid={fieldState.invalid}
-                {...field}
-              />
-            ) : null}
-            {variant === 'number' ? (
-              <Input
-                type="number"
-                id={name}
-                aria-invalid={fieldState.invalid}
-                {...field}
-                onChange={(e) => field.onChange(+e.target.value)}
-              />
-            ) : null}
-            {variant === 'select' && options ? (
-              <Select
-                aria-invalid={fieldState.invalid}
-                onValueChange={(value) => field.onChange(value)} // or convert value as needed
-                value={String(field.value)}
+                ref={field.ref}
+                onBlur={field.onBlur}
+                className="w-[180px]"
               >
-                <SelectTrigger
-                  id={name}
-                  ref={field.ref}
-                  onBlur={field.onBlur}
-                  className="w-[180px]"
-                >
-                  <SelectValue placeholder="Select option" />
-                </SelectTrigger>
-                <SelectContent>
-                  {options?.map(({ label, value }, i) => (
-                    <SelectItem key={i} value={value}>
-                      {label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            ) : null}
-            {fieldState.error && (
-              <FieldError>{fieldState.error.message}</FieldError>
-            )}
-          </>
-        )}
-      />
-    </Field>
+                <SelectValue placeholder="Select option" />
+              </SelectTrigger>
+              <SelectContent>
+                {options?.map(({ label, value }, i) => (
+                  <SelectItem key={i} value={value}>
+                    {label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          ) : null}
+          {fieldState.error && (
+            <FieldError>{fieldState.error.message}</FieldError>
+          )}
+        </Field>
+      )}
+    />
   );
 }
