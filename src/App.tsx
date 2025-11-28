c0b6dc06963c5ff3cd2cf713a363a9744915310c
xanoosh <xanoosh@gmail.com>
2025-11-28 07:17:41 +0100

    app title

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import ThemeToggler from '@/components/Theme/ThemeToggler';
 function App() {
   return (
     <ThemeProvider>
+      <title>QR Code Generator</title>
       <main className="h-screen flex flex-col gap-4 justify-center items-center bg-slate-100 dark:bg-stone-900 p-4">
         <ThemeToggler />
         <QrForm />
